50940bfa1e26009184984555b61d3b24f1d33d24
vansh.shah <vanshshah174@gmail.com>
2024-10-05 19:07:18 -0400

    Image_Upload Portion Implemented

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,30 @@
 import express from "express";
+import ImageKit from "imagekit"
+import cors from "cors";
 
 
-const port  = process.env.PORT || 3000;
+
+
+
+const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors({
+    origin: process.env.CLIENT_URL
+}))
 
-app.get("/test",(req,res)=>{
-    res.send("Hello World");
+const imageKit = new ImageKit({
+    urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+    publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
 })
 
-app.listen(port,()=>{
+
+app.get("/api/upload", (req, res) => {
+    const result = imageKit.getAuthenticationParameters();
+    res.send(result);
+})
+
+app.listen(port, () => {
     console.log("Server is running on port number 3000")
 })
--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -1,7 +1,15 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./newPrompt.css"
+import Upload from '../upload/Upload';
+import { IKImage } from 'imagekitio-react';
 
 const NewPrompt = () => {
+
+    const[img,setImg] = useState({
+      isLoading:false,
+      error:"",
+      dbData:{},
+    })
 
     const endRef = useRef(null);
 
@@ -13,12 +21,18 @@ const NewPrompt = () => {
   return (
     <>
     {/* ADD NEW CHAT */}
-    TEST
+    {img.isLoading && <div className=''>Loading.....</div>}
+    {img.dbData?.filePath && (
+      <IKImage
+      urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+      path={img.dbData?.filePath}
+      width="380"
+      transformation={[{ width: 380 }]}
+      />
+    )}
     <div className="endChat" ref={endRef}></div>
         <form className="newForm">
-            <label htmlFor="file">
-                <img src='/attachment.png' alt='' />
-            </label>
+            <Upload setImg={setImg} />
             <input type="file" id="file" multiple={false} hidden />
             <input type="text" placeholder='Ask anything......' />
             <button>
