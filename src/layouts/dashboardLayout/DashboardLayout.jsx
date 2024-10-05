6589b143aa714f3a885e329ff7222e52d46fe0f3
vansh.shah <vanshshah174@gmail.com>
2024-10-05 13:32:31 -0400

    Protected Routes implmented with clerk

--- src/layouts/dashboardLayout/DashboardLayout.jsx
+++ src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -1,8 +1,24 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import "./dashboardLayout.css";
+import { useAuth } from "@clerk/clerk-react";
 
 const DashboardLayout = () => {
+
+    const {userId,isLoaded} = useAuth()
+
+    const navigate =  useNavigate();
+
+    useEffect(()=>{
+
+        if(isLoaded && !userId){
+            navigate("/sign-in")
+        }
+    },[isLoaded,userId,navigate])
+
+
+    if(!isLoaded) return "Loading......";
+
   return (
     <div className="dashboardLayout">
       <div className="menu">MENU</div>
